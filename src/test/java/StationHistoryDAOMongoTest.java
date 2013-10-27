8caaf20dd4d80d6679866b64ada23e8022375897
Gabriel Cypriano <gabrielcs@gmail.com>
2013-10-27 03:13:00 -0200

    Using Travis' mongodb service

--- src/test/java/StationHistoryDAOMongoTest.java
+++ src/test/java/StationHistoryDAOMongoTest.java
@@ -10,13 +10,9 @@ import org.mongodb.morphia.Datastore;
 import org.mongodb.morphia.Morphia;
 
 import com.mongodb.MongoClient;
-import com.mongodb.MongoClientURI;
 
 public class StationHistoryDAOMongoTest {
-	private String mongoURI = "mongodb://admin:admin@paulo.mongohq.com:10092/morphia-query";
-	private String dbName = "morphia-query";
 	private Datastore datastore;
-
 	private static final ObjectId userId1 = new ObjectId(
 			"525c10fa45690266809924ed");
 	private static final ObjectId userId2 = new ObjectId(
@@ -26,14 +22,14 @@ public class StationHistoryDAOMongoTest {
 	public void test() {
 		StationHistoryDAOMongo stationHistoryEntryDAO = new StationHistoryDAOMongo(
 				datastore);
-
+		
 		// thumbs-up by user1
 		SongFeedback songFeedback1 = new SongFeedback(
 				SongFeedback.FeedbackType.THUMBS_UP, userId1);
 		StationHistoryEntry stationHistoryEntry1 = new StationHistoryEntry();
 		stationHistoryEntry1.addSongFeedback(songFeedback1);
 		stationHistoryEntryDAO.save(stationHistoryEntry1);
-
+		
 		// star by user1 and thumbs-up by user1
 		SongFeedback songFeedback2 = new SongFeedback(
 				SongFeedback.FeedbackType.STAR, userId1);
@@ -53,26 +49,20 @@ public class StationHistoryDAOMongoTest {
 		// query for StationHistoryEntry with star by user1
 		List<StationHistoryEntry> stationHistoryEntryList = stationHistoryEntryDAO
 				.findStarredByUserId(userId1);
-		// System.out.println(stationHistoryEntryList);
+		//System.out.println(stationHistoryEntryList);
 		assertEquals(1, stationHistoryEntryList.size());
 		assertEquals(stationHistoryEntry2, stationHistoryEntryList.get(0));
 	}
 
 	@Before
 	public void setUp() throws Exception {
-		MongoClientURI mongoClientURI = new MongoClientURI(mongoURI);
-		MongoClient mongoClient = new MongoClient(mongoClientURI);
-		datastore = new Morphia().createDatastore(mongoClient, dbName);
+		datastore = new Morphia().createDatastore(new MongoClient(), "morphia-query");
 	}
 
 	@After
 	public void tearDown() throws Exception {
 		if (datastore != null) {
-			// deletes all StationHistoryEntry documents
-			StationHistoryDAOMongo stationHistoryEntryDAO = new StationHistoryDAOMongo(
-					datastore);
-			stationHistoryEntryDAO.deleteByQuery(datastore
-					.find(StationHistoryEntry.class));
+			datastore.getDB().dropDatabase();
 		}
 	}
 }
